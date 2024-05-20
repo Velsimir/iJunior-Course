1ff19d3260265d16af0ec485b68923f3d7bbe4c0
Иван Коняшов <nintendocoin@yandex.ru>
2024-05-20 17:22:22 +0300

    Added BaseCode Folder

---
+++ C# practice/BaseCoding/1_variables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Threading;
+
+namespace iJunior
+{
+    class MainClass
+    {
+/*        Попрактикуйтесь в создании переменных.
+ *        Объявите 10 переменных и присвойте им значения.
+ *        Минимум 5 типов переменных должны быть разными.
+ *        Одна из переменных должна быть типа bool.*/
+
+        public static void Main(string[] args)
+        {
+            int price = 0;
+            int lives = 0;
+            float positionX = 0.1;
+            float positionY = 0.1;
+            double angleY = 0.1;
+            bool isAlive = true;
+            string name = "Player";
+            string tag = "Dog";
+            char symbol = '$';
+            char letter = 'A';
+        }
+    }
+}
