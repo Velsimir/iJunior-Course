e9866201efb1c2fdf4bc35c2b2419b34983adc2e
Иван Коняшов <nintendocoin@yandex.ru>
2024-05-24 19:25:40 +0300

    step

--- C# practice/Conditional operators and cycles/3_Sequence.cs
+++ C# practice/Conditional operators and cycles/3_Sequence.cs
@@ -5,10 +5,10 @@ public class Program
     public static void Main()
     {
         int maxValueOfTheCycle = 97;
-        int cycleStep = 7;
+        int step = 7;
         int minValue = 5;
 
-        for (int i = minValue; i < maxValueOfTheCycle; i += cycleStep)
+        for (int i = minValue; i < maxValueOfTheCycle; i += step)
         {
             Console.WriteLine(i);
         }
