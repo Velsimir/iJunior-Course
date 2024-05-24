2f85bdee198aa5dda6d4a0bc00ce54f67095cfbc
Иван Коняшов <nintendocoin@yandex.ru>
2024-05-24 18:18:15 +0300

    Fixed errors

--- C# practice/BaseCoding/6_Hospital.cs
+++ C# practice/BaseCoding/6_Hospital.cs
@@ -7,35 +7,23 @@ namespace iJunior
 {
     class MainClass
     {
-        /*Вы заходите в поликлинику и видите огромную очередь из пациентов, вам нужно рассчитать время ожидания в очереди.  
-         * Формально: 
-         * Пользователь вводит кол-во людей в очереди.  
-         * Фиксированное время приема одного человека всегда равно 10 минутам.  
-         * Пример ввода: Введите кол-во пациентов: 14
-         * Пример вывода: "Вы должны отстоять в очереди 2 часа и 20 минут."
-         * Примечание:
-         * - при расчетах надо использовать только переменные. 
-         * Если число не присваивается переменной, то в большинстве случаев это число магическое 
-         * (исключение 0 и 1, но не во всех ситуациях).
-         * - повторные расчеты так же стоит выносить в переменные*/
-
         public static void Main(string[] args)
         {
             int visitors = 0;
-            int receptionTime = 10;
-            int hours = 0;
-            int miutes = 0;
-            int totalWaitingTime = 0;
+            int receptionMinutes = 10;
+            int waitingHours = 0;
+            int waitingMinutes = 0;
+            int totalWaitingMinutes = 0;
             int minutesInOneHour = 60;
 
             Console.WriteLine($"Сколько перед вами людей в очереди?");
             visitors = Convert.ToInt32(Console.ReadLine());
 
-            totalWaitingTime = visitors * receptionTime;
-            hours = totalWaitingTime / minutesInOneHour;
-            miutes = totalWaitingTime % minutesInOneHour;
+            totalWaitingMinutes = visitors * receptionMinutes;
+            waitingHours = totalWaitingMinutes / minutesInOneHour;
+            waitingMinutes = totalWaitingMinutes % minutesInOneHour;
 
-            Console.WriteLine($"Вам осталось ожидать своей очереди {hours} Часов и {miutes} минут");
+            Console.WriteLine($"Вам осталось ожидать своей очереди {waitingHours} Часов и {waitingMinutes} минут");
         }
     }
 }
