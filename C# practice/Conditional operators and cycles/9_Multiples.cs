f63da9fbb81f2fe5e3efde75c47bf34cbb39f1ca
Иван Коняшов <nintendocoin@yandex.ru>
2024-05-30 22:14:06 +0300

    9_Multiples was Updated

--- C# practice/Conditional operators and cycles/9_Multiples.cs
+++ C# practice/Conditional operators and cycles/9_Multiples.cs
@@ -6,19 +6,23 @@ namespace iJunior
     {
         public static void Main(string[] args)
         {
-            Random random = new Random();
+        	Random random = new Random();
+        
+        	int smallestNumber = 10;
+        	int highestNumber = 26;
+        	int devider = random.Next(smallestNumber, highestNumber);
+        	int quantityOfDivisibleNumbers = 0;
+        	int maxValue = 150;
+        	int minValue = 50;
 
-            int smallestNumber = 1;
-            int highestNumber = 27;
-            int devider = random.Next(smallestNumber, highestNumber);
-            int quantityOfDivisibleNumbers = 0;
-            int maxValue = 999;
-            int minValue = 100;
+        	for (int i = minValue; i <= maxValue; i += devider)
+        	{
+            	if (i < maxValue)
+                	quantityOfDivisibleNumbers += 1;
+        	}
 
-            for (int i = 1; i <= maxValue; i += devider)
-                quantityOfDivisibleNumbers += i >= minValue ? 1 : 0;
-
-            Console.WriteLine($"{devider}, {quantityOfDivisibleNumbers}");
+        	Console.WriteLine($"Количество чисел N ({devider}) в диапазоне" +
+            	              $" от 50 до 150 = {quantityOfDivisibleNumbers}");
         }
     }
 }
