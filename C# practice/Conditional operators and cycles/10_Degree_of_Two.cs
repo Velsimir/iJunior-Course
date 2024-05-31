3e9c7a295d31d213c1384d6b49d6a2182a31df42
Иван Коняшов <nintendocoin@yandex.ru>
2024-05-31 11:41:40 +0300

    10_Degree_of_Two was updated

--- C# practice/Conditional operators and cycles/10_Degree_of_Two.cs
+++ C# practice/Conditional operators and cycles/10_Degree_of_Two.cs
@@ -7,23 +7,17 @@ namespace iJunior
         public static void Main(string[] args)
         {
             Random random = new Random();
-            int originalNumber = random.Next(0, 100);
+            int minValue = 0;
+            int maxValue = 100;
+            int originalNumber = random.Next(minValue, maxValue);
             int numberRaisedToPower = 1;
             int degree = 2;
             int numberInPower = 0;
-            bool isWorking = true;
 
-            while (isWorking)
+            while (originalNumber > numberRaisedToPower)
             {
-                if (originalNumber > numberRaisedToPower)
-                {
-                    numberRaisedToPower *= degree;
-                    numberInPower += 1;
-                }
-                else
-                {
-                    isWorking = false;
-                }
+                numberRaisedToPower *= degree;
+                numberInPower += 1;
             }
 
             Console.WriteLine($"Изначальное число - {originalNumber}" +
