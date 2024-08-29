4f5c988741f0b32bab85bed530e72ea38cdae8b2
Velsimir <Velsimir@gmail.com>
2024-08-29 19:49:41 +0300

    Updated

--- C# practice/Arrays/3_Local_maximum.cs
+++ C# practice/Arrays/3_Local_maximum.cs
@@ -10,7 +10,6 @@ namespace iJunior
             {23, 45, 24, 65, 34, 76, 34, 65, 54, 87,
                 324, 432, 324, 432, 654, 453, 234, 431, 688, 312,
                 634, 536, 457, 133, 876, 456, 234, 654, 967, 1000};
-            int localMax;
             int previousNumber;
             int currentNumber;
             int nextNumber;
@@ -18,10 +17,7 @@ namespace iJunior
             int penultimateNumber = numbers[arrayLength - 2];
 
             if (numbers[0] > numbers[1])
-            {
-                localMax = numbers[0];
-                Console.WriteLine(localMax);
-            }
+                Console.WriteLine(numbers[0]);
 
             for (int i = 1; i < arrayLength - 1; i++)
             {
@@ -30,17 +26,11 @@ namespace iJunior
                 previousNumber = numbers[i - 1];
 
                 if (currentNumber >= previousNumber && currentNumber >= nextNumber)
-                {
-                    localMax = currentNumber;
-                    Console.WriteLine(localMax);
-                }
+                    Console.WriteLine(currentNumber);
             }
 
             if (numbers[arrayLength - 1] > penultimateNumber)
-            {
-                localMax = numbers[arrayLength - 1];
-                Console.WriteLine(localMax);
-            }
+                Console.WriteLine(numbers[arrayLength - 1]);
         }
     }
 }
