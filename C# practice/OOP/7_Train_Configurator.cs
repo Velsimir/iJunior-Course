c2994b9cb9048231d01bc80d2dd3da47d0734cfa
Velsimir <nintendocoin@yandex.ru>
2024-09-21 16:50:14 +0300

    Updated

--- C# practice/OOP/7_Train_Configurator.cs
+++ C# practice/OOP/7_Train_Configurator.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
-using System.Runtime.CompilerServices;
 
 namespace iJunior
 {
@@ -15,13 +13,13 @@ namespace iJunior
 
             bool isWorking = true;
             string userInput;
-            Station station = new Station();
+            Dispetcher dispetcher = new Dispetcher();
 
             while (isWorking)
             {
                 Console.Clear();
 
-                station.ShowInfo();
+                dispetcher.ShowInfo();
 
                 Console.WriteLine("Хотите принять пассажиров на рейс?");
                 userInput = Console.ReadLine();
@@ -29,7 +27,7 @@ namespace iJunior
                 switch (userInput)
                 {
                     case CommandCreateRace:
-                        station.CreateRoute();
+                        dispetcher.CreateRoute();
                         break;
 
                     case CommandExit:
@@ -43,19 +41,19 @@ namespace iJunior
             }
         }
     }
-
+        
     class RailwayCarriage
     {
         private List<int> _passengers = new List<int>();
         private int _minPlaces = 10;
         private int _maxPlaces = 20;
 
-        public int FreePlaces { get; private set; }
-
         public RailwayCarriage()
         {
             FreePlaces = new Random().Next(_minPlaces, _maxPlaces);
         }
+
+        public int FreePlaces { get; private set; }
 
         public void SeatPassengers(int passengers)
         {
@@ -111,10 +109,6 @@ namespace iJunior
     {
         private Train _train;
 
-        public int Id { get; private set; }
-        public int CountOfPassengers { get; private set; }
-        public Direction Direction { get; private set; }
-
         public Route(int id, Direction direction, int passengers)
         {
             Id = id;
@@ -123,13 +117,17 @@ namespace iJunior
             _train = new Train(passengers);
         }
 
+        public int Id { get; private set; }
+        public int CountOfPassengers { get; private set; }
+        public Direction Direction { get; private set; }
+
         public int GetCountRailwayCarriages()
         {
             return _train.GetCountRailwayCarriages();
         }
     }
 
-    class Station
+    class Dispetcher
     {
         private List<Route> _routes = new List<Route>();
         private int _idTrain = 1;
@@ -146,31 +144,14 @@ namespace iJunior
 
         public void ShowInfo()
         {
-            int column1 = 0;
-            int column2 = 16;
-            int column3 = 40;
-            int column4 = 72;
-            int line = 1;
-
             Console.WriteLine("Рейс\t\tНаправление\t\tКоличество вагонов\t\tКоличество пассажиров");
 
             if (_routes.Count() > 0)
             {
                 foreach (var route in _routes)
                 {
-                    Console.SetCursorPosition(column1, line);
-                    Console.Write($"{route.Id}");
-
-                    Console.SetCursorPosition(column2, line);
-                    Console.Write($"{route.Direction.From}-{route.Direction.To}");
-
-                    Console.SetCursorPosition(column3, line);
-                    Console.Write($"{route.GetCountRailwayCarriages()}");
-
-                    Console.SetCursorPosition(column4, line);
-                    Console.WriteLine($"{route.CountOfPassengers}");
-
-                    line++;
+                    Console.WriteLine($"{route.Id}\t\t{route.Direction.From}-{route.Direction.To}" +
+                                      $"\t\t{route.GetCountRailwayCarriages()}\t\t{route.CountOfPassengers}");
                 }
             }
             else
@@ -200,23 +181,23 @@ namespace iJunior
         private int _minPassengers = 1;
         private int _maxPassengers = 1001;
 
-        public int Passengers { get; private set; }
-
         public int Generate()
         {
             return Passengers = _random.Next(_minPassengers, _maxPassengers);
         }
+        
+        public int Passengers { get; private set; }
     }
 
     struct Direction
     {
-        public string From { get; private set; }
-        public string To { get; private set; }
-
         public Direction(string from, string to)
         {
             From = from;
             To = to;
         }
+        
+        public string From { get; private set; }
+        public string To { get; private set; }
     }
 }
